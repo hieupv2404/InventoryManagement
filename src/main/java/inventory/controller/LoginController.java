0209eb78091f764efa9df27278046ad73e993827
Hieupv <hieupv>
2020-08-13 22:48:16 +0700

    script

--- src/main/java/inventory/controller/LoginController.java
+++ src/main/java/inventory/controller/LoginController.java
@@ -3,6 +3,7 @@ package inventory.controller;
 import inventory.model.*;
 import inventory.service.UserService;
 import inventory.util.Constant;
+import inventory.util.HashingPassword;
 import inventory.validate.LoginValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,8 +29,6 @@ public class LoginController {
     @Autowired
     private LoginValidator loginValidator;
 
-    private int check =1;
-
     @InitBinder
     private void initBinder(WebDataBinder binder) {
         if (binder.getTarget() == null) return;
@@ -40,26 +39,18 @@ public class LoginController {
 
     @GetMapping("/login")
     //van chuyen du lieu tu backend len tang view. cua spring ui
-    public String login(Model model, HttpSession session) {
+    public String login(Model model) {
         List<Users> users = userService.findByProperty("status", 1);
         for (Users user : users) {
             user.setStatus(0);
             userService.updateStatus(user);
         }
-
-
-
         model.addAttribute("loginForm", new Users());
-        if(check==2)
-            session.setAttribute(Constant.MSG_ERROR, "PassWord is wrong!!!");
-        if (check==3)
-            session.setAttribute(Constant.MSG_ERROR, "UserName doesn't exist!!!");
         return "login/login";
     }
 
     @PostMapping("/processLogin")
     public String processLogin(Model model, @ModelAttribute("loginForm") @Validated Users users, BindingResult result, HttpSession session) {
-
         if (result.hasErrors()) {
             return "login/login";
         }
@@ -67,54 +58,51 @@ public class LoginController {
 
         if (userList.size() != 0) {
 
-                Users user = userList.get(0);
-                if (users.getPassword().equals(user.getPassword())) {
-                    UserRole userRole = (UserRole) user.getUserRoles().iterator().next();
-                    List<Menu> menuList = new ArrayList<>();
-                    Role role = userRole.getRole();
-                    List<Menu> menuChildList = new ArrayList<>();
-                    for (Object obj : role.getAuths()) {
-                        Auth auth = (Auth) obj;
-                        Menu menu = auth.getMenu();
-                        if (menu.getParentId() == 0 && menu.getOrderIndex() != -1 && menu.getActiveFlag() == 1 && auth.getPermission() == 1 && auth.getActiveFlag() == 1) {
-                            menu.setIdMenu(menu.getUrl().replace("/", "") + "Id");
-                            menuList.add(menu);
-                        } else if (menu.getParentId() != 0 && menu.getOrderIndex() != -1 && menu.getActiveFlag() == 1 && auth.getPermission() == 1 && auth.getActiveFlag() == 1) {
-                            menu.setIdMenu(menu.getUrl().replace("/", "") + "Id");
-                            menuChildList.add(menu);
-                        }
+            Users user = userList.get(0);
+            String passMD = HashingPassword.encrypt(users.getPassword());
+            if (passMD.equals(user.getPassword())) {
+                UserRole userRole = (UserRole) user.getUserRoles().iterator().next();
+                List<Menu> menuList = new ArrayList<>();
+                Role role = userRole.getRole();
+                List<Menu> menuChildList = new ArrayList<>();
+                for (Object obj : role.getAuths()) {
+                    Auth auth = (Auth) obj;
+                    Menu menu = auth.getMenu();
+                    if (menu.getParentId() == 0 && menu.getOrderIndex() != -1 && menu.getActiveFlag() == 1 && auth.getPermission() == 1 && auth.getActiveFlag() == 1) {
+                        menu.setIdMenu(menu.getUrl().replace("/", "") + "Id");
+                        menuList.add(menu);
+                    } else if (menu.getParentId() != 0 && menu.getOrderIndex() != -1 && menu.getActiveFlag() == 1 && auth.getPermission() == 1 && auth.getActiveFlag() == 1) {
+                        menu.setIdMenu(menu.getUrl().replace("/", "") + "Id");
+                        menuChildList.add(menu);
                     }
-                    for (Menu menu : menuList) {
-                        List<Menu> childList = new ArrayList<>();
-                        for (Menu childMenu : menuChildList) {
-                            if (childMenu.getParentId() == menu.getId()) {
-                                childList.add(childMenu);
-                            }
-                        }
-                        menu.setChild(childList);
-                    }
-                    sortMenu(menuList);
-                    for (Menu menu : menuList) {
-                        sortMenu(menu.getChild());
-                    }
-                    user.setStatus(1);
-                    userService.updateStatus(user);
-                    session.setAttribute(Constant.MENU_SESSION, menuList);
-                    session.setAttribute(Constant.USER_INFO, user);
-
-
-                    session.setAttribute(Constant.MSG_SUCCESS, "Login Successful!");
-                    return "redirect:/index";
-
-                } else {
-                    session.setAttribute(Constant.MSG_ERROR, "PassWord is wrong!!!");
-                    check=2;
-                    return "redirect:/login";
                 }
+                for (Menu menu : menuList) {
+                    List<Menu> childList = new ArrayList<>();
+                    for (Menu childMenu : menuChildList) {
+                        if (childMenu.getParentId() == menu.getId()) {
+                            childList.add(childMenu);
+                        }
+                    }
+                    menu.setChild(childList);
+                }
+                sortMenu(menuList);
+                for (Menu menu : menuList) {
+                    sortMenu(menu.getChild());
+                }
+                user.setStatus(1);
+                userService.updateStatus(user);
+                session.setAttribute(Constant.MENU_SESSION, menuList);
+                session.setAttribute(Constant.USER_INFO, user);
+                session.setAttribute(Constant.MSG_SUCCESS, "Login Successful!!!");
+                return "redirect:/index";
+
+            } else {
+                session.setAttribute(Constant.MSG_ERROR, "PassWord is wrong!!!");
+                return "redirect:/login";
+            }
 
         } else {
             session.setAttribute(Constant.MSG_ERROR, "UserName doesn't exist!!!");
-            check=3;
             return "redirect:/login";
         }
     }
